37812aa1ab1a1842fcec6ebcc23fe85ea18cc746
Abhijith <abhijithpv32@gmail.com>
2025-06-16 23:18:59 +0530

    Update main.cpp

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -16,7 +16,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // Initialize COM, so that it is available for use in the library and/or
   // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
-
+ 
   flutter::DartProject project(L"data");
 
   std::vector<std::string> command_line_arguments =
